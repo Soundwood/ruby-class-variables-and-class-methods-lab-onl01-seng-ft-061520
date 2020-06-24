fd0607d529ea8de799abb1be2dc1823cb45cedfc
Ben Hogan <j.benhogan@gmail.com>
2020-06-24 01:57:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,7 @@ class Song
   def self.artist_count
     artist_count_hash = {}
     @@artists.each do |artist|
+      binding.pry 
       if artist_count_hash[artist] == nil 
         artist_count_hash[artist] = 1  
       else
